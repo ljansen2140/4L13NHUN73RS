1d1b9cee4684dc658467dd706183270d3ce8cef4
ysvar <66091297+ysvar@users.noreply.github.com>
2020-11-19 15:37:16 -0600

    Update vae_code.py

--- vae_code.py
+++ vae_code.py
@@ -3,29 +3,67 @@
 
 ############################ Yvannia ################################
 
+# Importing the package --> "numpy" so that all the objects defined in the module can be used. Imported as "np" for easy referral (renaming)
 import numpy as np
+# Importing the package --> "matplotlib.pyplot" so that all the objects defined in the module can be used. Imported as "plt" for easy referral (renaming)
 import matplotlib.pyplot as plt
+# Importing the package --> "tensorflow" so that all the objects defined in the module can be used. Imported as "tf" for easy referral (renaming)
 import tensorflow as tf
+# Importing a specific object named "input_data" to have shorter calls later on in the code 
 from tensorflow.examples.tutorials.mnist import input_data
 
+# contrib makes it easier to configure, train, and evaluate a variety of machine learning models
+# distributions is a class that is used for constructing and organizing properties (mean, variance, standard deviation, etc.) of variables 
+# Here it is shortened to "tfd" to avoid long code 
 tfd = tf.contrib.distributions
 
 
+# Encoder function --------------------------------------------------------------------------------------------------------------
+# Function definition of the encoder that takes in two parameters, data and code_size 
 def make_encoder(data, code_size):
+
+# Layers is an object that involves computation --> it is an important method used to create a neural network 
+# Flatten is used to compress the input, which in this case is the parameter "data"
+# Flatten is applied to the layer having a shape such as (size, 2,2), and then outputs the shape as (size, 4). i.e. collapses spatial dimensions 
   x = tf.layers.flatten(data)
+
+# There are two identical calls because the first is a hidden layer (where every node is connected to every other node in the next layer) 
+# and the second is an output layer (the last layer of the node connections). 
+# Dense is a network layer. It feeds all outputs from the previos layer, to all its neurons. 
+# x is the previous layers output defined above and 200 is the number of neurons. 
+# nn stands for neural network --> provides support for many basic neural network operations 
+# relu stands for Rectified Linear Unit which is a function --> it is computationally faster and allows for fewer vanishing gradients. 
   x = tf.layers.dense(x, 200, tf.nn.relu)
   x = tf.layers.dense(x, 200, tf.nn.relu)
+  
+# Same as above, but instead of taking the parameter 200 it takes in the input "code_size"
+# loc is the mean 
   loc = tf.layers.dense(x, code_size)
+  
+# Same as above but now using "softplus" --> provides more stabilization and performance to deep neural network (than ReLU function).
+# scale is the standard deviation. 
   scale = tf.layers.dense(x, code_size, tf.nn.softplus)
+
+# Implement multivariate normal distributions with a diagonal covariance structure.
+# Takes in parameter loc = mean and scale = standard deviation. 
+# Returns a graph of the distribution that will be compared against what it should look like later. 
   return tfd.MultivariateNormalDiag(loc, scale)
 
 
+# Make_Prior function --------------------------------------------------------------------------------------------------------------
+# Function definition of make_prior that takes in one parameters, code_size. 
 def make_prior(code_size):
+# tf.zeros creates a tensor (a multi-dimensional array with a uniform type) with all elements of the input "code_size" set to zero.  
   loc = tf.zeros(code_size)
+# tf.ones creates a tensor with all elements of the input "code_size" set to one. 
   scale = tf.ones(code_size)
+# Same explanation as above
+# Returns a graph of what the distribution should look like that is compared against the returned graph above. 
   return tfd.MultivariateNormalDiag(loc, scale)
 
 ######################################################################
+
+
 
 ############################ Botoul ##################################
 
@@ -112,6 +150,7 @@ optimize = tf.train.AdamOptimizer(0.001).minimize(-elbo)
 samples = make_decoder(prior.sample(10), [28, 28]).mean()
 
 ######################################################################
+
 
 ############################ Sean ####################################
 #Simplifies the input data into mnist
